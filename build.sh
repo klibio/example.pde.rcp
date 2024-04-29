9cfa1695d32092c157a4501e3134cbdcf191ead7
Peter Kirschner <peter@klib.io>
2024-04-29 21:01:36 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -45,10 +45,7 @@ log_file=_log/${ts}_build.log
 echo -e "#\n# build - log file $log_file\n#\n"      2>&1 | tee -a $log_file
 
 echo -e "#\n# sourcing environment from build\n#\n" 2>&1 | tee -a $log_file
-pushd ~/.klibio
-ls -la
-set-java.sh 17 # must not use pipe `2>&1 | tee -a $log_file`, cause export env vars will not work
-popd
+. ~/.klibio/set-java.sh 17 # must not use pipe `2>&1 | tee -a $log_file`, cause export env vars will not work
 
 # signing 
 echo -e "#\n# sourcing sign properties\n#\n" 2>&1 | tee -a $log_file
