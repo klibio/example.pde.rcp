8fbedc7f541ff3135a34395792fd30b8eac71893
Peter Kirschner <peter@klib.io>
2024-05-20 19:29:15 +0200

    Update setup-idefix.sh

--- .github/workflows/setup-idefix.sh
+++ .github/workflows/setup-idefix.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+002-setup-idefix.sh#!/bin/bash
 if [[ ${debug:-false} == true ]]; then
     set -o xtrace   # activate bash debug
 fi
@@ -10,7 +10,13 @@ if [ -d ~/${bootstrap} ]; then
   echo "using existing idefix tooling from ~/${bootstrap} - skipping provisioning" 
 else
   echo "provisioning idefix tooling inside ~/${bootstrap} - please be patient"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/klibio/bootstrap/main/install-klibio.sh)" bash -j
+#  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/klibio/bootstrap/main/install-klibio.sh)" bash -j
+  export KLIBIO=$(echo ${HOME}/.klibio)
+  lib_url=https://raw.githubusercontent.com/klibio/bootstrap/main/.klibio/klibio.sh
+  echo "# sourcing klibio library - ${lib_url}"
+  install_dir=${HOME}
+  $(curl -fs${unsafe:-}SLO ${lib_url})
+  . klibio.sh -j
 fi
 
 echo execution completed at $(date +"%Y-%m-%d_%H-%M-%S")
