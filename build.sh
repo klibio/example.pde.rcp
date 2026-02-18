8be6410e56d9f921bb764328f40cc926db7111c7
Peter Kirschner <kirschnersgmbh.kirschner@extaccount.com>
2026-02-18 15:37:37 +0100

    update to tycho 5.0.2
    Signed-off-by: Peter Kirschner <kirschnersgmbh.kirschner@extaccount.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -o pipefail
+
 if [[ ${debug:-false} == true ]]; then
     set -o xtrace   # activate bash debug
     mvnDebug="-X"
@@ -83,7 +85,9 @@ echo -e "# end of signing properties\n" 2>&1 | tee -a $log_file
 
 if [[ ${diag} -eq 1 ]]; then
     echo -e "#\n# execute diagnose\n#\n" 2>&1 | tee -a $log_file
+    set -e
     ./mvnw \
+        -ntp \
         ${MAVEN_OPTS} \
         ${mvnDebug} \
         ${gpg_signing} \
@@ -94,7 +98,7 @@ fi
 if [[ ${build_local} -eq 1 ]]; then
     echo -e "#\n# execute local build - log file $log_file\n#\n"  2>&1 | tee -a $log_file
 read -r -d '' command << EOM
-./mvnw clean verify \
+./mvnw -ntp clean verify \
   ${MAVEN_OPTS} \
   ${mvnDebug} \
   ${local_cache} \
@@ -110,7 +114,7 @@ fi
 if [[ ${build_deploy} -eq 1 ]]; then
     echo -e "#\n#\n# execute SNAPSHOT/RELEASE build - log file $log_file\n#\n#\n"  2>&1 | tee -a $log_file
 read -r -d '' command << EOM
-./mvnw clean deploy -Ddeploy-maven-to-klibio=true \
+./mvnw -ntp clean deploy -Ddeploy-maven-to-klibio=true \
   ${MAVEN_OPTS} \
   ${mvnDebug} \
   ${local_cache} \
